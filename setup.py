3f81848f798d7b72fb49bf9bec4d8fbecc4040a6
girish12n <girish12n@gmail.com>
2023-09-03 14:20:34 +0530

    requirements installation done

--- setup.py
+++ setup.py
@@ -1,1 +1,24 @@
-#rehfslkflksafkskf
+import setuptools
+
+with open('README.md','r',encoding='utf-8') as f:
+    long_description=f.read()
+
+__version__="0.0.0"
+REPO_NAME="NLP-Text-Summarization"
+SRC_REPO="NLP-PROJECT"
+AUTHOR_USER_NAME="girish12ns"
+AUTHOR_EMAIL:"girish12n@gmail.com"
+
+setuptools.setup(
+    name=SRC_REPO,
+    version=__version__,
+    long_description=long_description,
+    author=AUTHOR_USER_NAME,
+    url=f"https://github.com/{AUTHOR_USER_NAME}{REPO_NAME}",
+    project_urls={
+        "Bug Tracker": f"https://github.com/{AUTHOR_USER_NAME}{REPO_NAME}/issues"
+
+    },
+    package_dir= {"" :"src"},
+    packages=setuptools.find_packages(where="src")
+)
